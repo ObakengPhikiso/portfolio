ee2c81156adb3b17dbc0456ec0022aeb5b1bc001
obakengphikis <obakeng.phikiso@gmail.com>
2020-02-15 20:49:31 +0200

    Fixed the responsiveness of the background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, state, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state('flyIn', style({
+        transform: 'translateX(0)'
+      })),
+      transition(':enter', [
+        style({transform: 'translateX(-100%)'}),
+        animate('0.5s 300ms ease-in')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'portfolio';
 }
